199111e801fc18b933b21a720dbfa6f422e135da
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-03-21 20:47:00 -0500

    [modification main method]

--- Gatos_APP/src/main/java/com/platzi/Main.java
+++ Gatos_APP/src/main/java/com/platzi/Main.java
@@ -1,9 +1,10 @@
 package com.platzi;
 
 import javax.swing.*;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int opcion_menu = -1;
         String [] botones = {"1. ver gatos", "2. salir."};
 
