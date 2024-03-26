71e83955e502b6689ab33e3e49e3696bbaf599f1
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-03-25 22:18:30 -0500

    [Add favourites Cats]
    agregamos la clase de gatos favoritos para luego solicitar cuadno queremos mirar los gatos que hemos guardado como favoritos.

---
+++ Gatos_APP/src/main/java/com/platzi/GatosFav.java
@@ -0,0 +1,41 @@
+package com.platzi;
+
+public class GatosFav {
+    //parametros para parcear la respuesta
+    String id;
+    String image_id;
+    String apikey = "live_OFqRpUKxbKW41RqavnLLeLLscJpEUzfnmvwOuCZ8M7UwlTyADhNNEwk9Xx9iwq1q";
+    public Imagex image;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImage_id() {
+        return image_id;
+    }
+
+    public void setImage_id(String image_id) {
+        this.image_id = image_id;
+    }
+
+    public String getApikey() {
+        return apikey;
+    }
+
+    public void setApikey(String apikey) {
+        this.apikey = apikey;
+    }
+
+    public Imagex getImage() {
+        return image;
+    }
+
+    public void setImage(Imagex image) {
+        this.image = image;
+    }
+}
