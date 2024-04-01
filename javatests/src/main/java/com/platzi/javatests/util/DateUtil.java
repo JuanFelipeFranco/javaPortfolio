95e16a1ffd37b6cb5d8d532726fd12709c5d5a9b
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-04-01 16:10:14 -0500

    [TDD: calculing Leap Year]
    Aplicamos metodo TDD:
    -Escribir test que no pase.
    -Escribir el codigo que aprueba el test.

---
+++ javatests/src/main/java/com/platzi/javatests/util/DateUtil.java
@@ -0,0 +1,17 @@
+package com.platzi.javatests.util;
+
+public class DateUtil {
+
+    public static boolean isLeapYear(int year){
+        if (year % 4 == 0 && year % 100 !=0){
+            return true;
+        }
+        if (year % 400 == 0 ){
+            return true;
+        }else {
+            return false;
+        }
+
+
+    }
+}
