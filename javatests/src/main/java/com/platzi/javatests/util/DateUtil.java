4a74f1bde5eaaeaae6ba747c30786f0237a6aa4c
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-04-01 16:12:27 -0500

    [TDD: Refactor class]
    Refactor: Aprobado el test; mejoramos nuestra clase.

--- javatests/src/main/java/com/platzi/javatests/util/DateUtil.java
+++ javatests/src/main/java/com/platzi/javatests/util/DateUtil.java
@@ -3,12 +3,10 @@ package com.platzi.javatests.util;
 public class DateUtil {
 
     public static boolean isLeapYear(int year){
-        if (year % 4 == 0 && year % 100 !=0){
+        if ((year % 4 == 0 && year % 100 !=0) || year % 400 == 0){
             return true;
         }
-        if (year % 400 == 0 ){
-            return true;
-        }else {
+        else {
             return false;
         }
 
