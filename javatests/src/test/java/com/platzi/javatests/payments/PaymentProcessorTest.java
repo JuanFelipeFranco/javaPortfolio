afcd4c5f60c82d1009d83cd193861b38f110ad11
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-04-01 12:12:37 -0500

    [Optimization to tests]
    creamos la clase setup donde va todas las variables que se repiten en los test; y le colocamos el before que nos avisa que se va ejecutar antes de cada test; por ultimo aplicamos los tres partes de los test (ARRAGE- ACT-ASSERT)

--- javatests/src/test/java/com/platzi/javatests/payments/PaymentProcessorTest.java
+++ javatests/src/test/java/com/platzi/javatests/payments/PaymentProcessorTest.java
@@ -1,5 +1,6 @@
 package com.platzi.javatests.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -7,35 +8,48 @@ import org.mockito.Mockito;
 import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
-
+    //Estas variables deben tenerse como campos de clase.
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+    @Before //metodo que le dice a JUnit que antes de ejecutar cada test ejecuta esta parte de setup
+    public void setup(){
+        paymentGateway= Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
     @Test
     public void payment_is_correct(){
         //simulamos el paymentgateway que lo pasaremos al procesador
-        PaymentGateway paymentGateway= Mockito.mock(PaymentGateway.class);
+//        PaymentGateway paymentGateway= Mockito.mock(PaymentGateway.class);
 
         //simulamos con mokito que cuando al paymentgateway se le haga un request de algun payment,
         // cualquier tipo de request el va devolver una respuesta correcta.
+        //----ARRANGE---- PREPAPARACION que queremos probar
         Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(
                 new PaymentResponse(PaymentResponse.PaymentStatus.OK
         ));
 
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
+//        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         //comprobamos que cuando al procesor intenta crear el pago de cierta cantidad el resultado es correcto
         assertTrue(paymentProcessor.makePayment(1500));
     }
 
+    //
     @Test
     public void payment_is_wrong(){
-
-        PaymentGateway paymentGateway= Mockito.mock(PaymentGateway.class);
-
+        //----------PREPARACION DE LOS OBJETOS-------------
+//        PaymentGateway paymentGateway= Mockito.mock(PaymentGateway.class);
         //simulamos que cuando se le haga un request al getway el va devolver ERROR
         Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(
                 new PaymentResponse(PaymentResponse.PaymentStatus.ERROR ));
         //el payment processor deberia decir que es false. Que el pago no se ha realizado correcto.
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
-        assertFalse(paymentProcessor.makePayment(1500));
+//        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
+
+        //--------EJECUCION DEL METODO - probando el metodo makepayment: nuestro objetivo----------
+        boolean result = paymentProcessor.makePayment(1500);
+
+        //----------COMPROBACION DEL RESULTADO-----------
+        assertFalse(result);
     }
 
 }
--- javatests/src/test/java/com/platzi/javatests/util/PasswordUtilTest.java
+++ javatests/src/test/java/com/platzi/javatests/util/PasswordUtilTest.java
@@ -8,26 +8,26 @@ import static org.junit.Assert.*;
 public class PasswordUtilTest {
     @Test
     public void weak_when_has_less_than_8_letters(){
-        //comprobar que un pass es debil cuando tiene menos de 8 letras.
+        //comprobar que un pass es debil cuando tiene menos de 8 digitos.
         assertEquals(WEAK,PasswordUtil.assessPassword("1234567"));
     }
 
     @Test
     public void weak_when_has_only_letters(){
-        //comprobar que un pass es debil cuando tiene 8 letras.
+        //comprobar que un pass es debil cuando tiene solo letras.
         assertEquals(WEAK,PasswordUtil.assessPassword("abcdefgh"));
     }
 
     @Test
     public void medium_when_has_letters_and_numbers(){
-        //comprobar que un pass es debil cuando tiene 8 letras.
+        //comprobar que un pass es medio cuando tiene letras y numeros y son mayor o igual a 8 digitos .
         assertEquals(MEDIUM,PasswordUtil.assessPassword("abcd1234"));
     }
 
     @Test
     public void strong_when_has_letters_numbers_and_symbols(){
-        //comprobar que un pass es debil cuando tiene 8 letras.
-        assertEquals(STRONG,PasswordUtil.assessPassword("abcd1234!"));
+        //comprobar que un pass es fuerte cuando tiene mas de 8 digitos, contiene letras numeros y digitos.
+        assertEquals(STRONG,PasswordUtil.assessPassword("abcd12340$"));
     }
 
 
