efbd770520749844e8bf0109394608f6c1704618
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-05-08 18:49:34 -0500

    [READ DATA FROM JAVA]

--- JAVA SE SQL AND DATABASES/ProyectoCRUD/src/main/java/org/exaple/felipe/main/Main.java
+++ JAVA SE SQL AND DATABASES/ProyectoCRUD/src/main/java/org/exaple/felipe/main/Main.java
@@ -1,27 +1,19 @@
 package org.exaple.felipe.main;
 
+import org.exaple.felipe.model.Employee;
+import org.exaple.felipe.repository.EmployeeRepository;
+import org.exaple.felipe.repository.Repository;
 import org.exaple.felipe.util.DatabaseConnection;
 
 import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) {
-        try (
-                //1. conexion a nuestra base de datos, pasando 3 argumentos
-                Connection myConn = DatabaseConnection.getInstance();
-                //2. Crear una declaracion objeto
-                Statement myStamt = myConn.createStatement();
-                //3. Ejecutar consulta SQL
-                ResultSet myRes = myStamt.executeQuery("SELECT * FROM employees");){
-
-                while (myRes.next()) {
-                    System.out.println(myRes.getString("first_name"));
-                }
+    public static void main(String[] args) throws SQLException {
+        try (Connection myConn = DatabaseConnection.getInstance()){
+            Repository<Employee> repository = new EmployeeRepository();
+            repository.findAll().forEach(System.out::println);
         }
-                catch (Exception e) {
-                    e.printStackTrace();
-                    System.out.println("algo salio mal");
-                }
-            }
- }
 
+
+ }
+}
