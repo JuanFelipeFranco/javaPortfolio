a3d0c2a2ed4734a57c987613d58460dc3584c9f0
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-05-08 17:25:39 -0500

    [Update Main]

--- JAVA SE SQL AND DATABASES/ProyectoCRUD/src/main/java/org/exaple/felipe/main/Main.java
+++ JAVA SE SQL AND DATABASES/ProyectoCRUD/src/main/java/org/exaple/felipe/main/Main.java
@@ -1,20 +1,18 @@
 package org.exaple.felipe.main;
 
+import org.exaple.felipe.util.DatabaseConnection;
+
 import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-         String url = "jdbc:mysql://localhost:3306/project";
-         String user = "root";
-         String pass = "admin";
-
         try (
-            //1. conexion a nuestra base de datos, pasando 3 argumentos
-            Connection myConn = DriverManager.getConnection(url, user, pass);
-            //2. Crear una declaracion objeto
-            Statement myStamt = myConn.createStatement();
-            //3. Ejecutar consulta SQL
-            ResultSet myRes = myStamt.executeQuery("SELECT * FROM employees");){
+                //1. conexion a nuestra base de datos, pasando 3 argumentos
+                Connection myConn = DatabaseConnection.getInstance();
+                //2. Crear una declaracion objeto
+                Statement myStamt = myConn.createStatement();
+                //3. Ejecutar consulta SQL
+                ResultSet myRes = myStamt.executeQuery("SELECT * FROM employees");){
 
                 while (myRes.next()) {
                     System.out.println(myRes.getString("first_name"));
