15368040cacccaa95464e0830e78fd9f660c0d1b
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-05-08 21:40:21 -0500

    [ADD UPDATE employee]

--- JAVA SE SQL AND DATABASES/ProyectoCRUD/src/main/java/org/exaple/felipe/main/Main.java
+++ JAVA SE SQL AND DATABASES/ProyectoCRUD/src/main/java/org/exaple/felipe/main/Main.java
@@ -11,9 +11,32 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         try (Connection myConn = DatabaseConnection.getInstance()){
             Repository<Employee> repository = new EmployeeRepository();
+            System.out.println("-----------LISTANDO EMPLEADOS--------");
             repository.findAll().forEach(System.out::println);
             System.out.println("--------------------GET BY ID------------------");
             System.out.println(repository.getById(3));
+            /*System.out.println("--------------------INSERTANO EMPLEADO------------------");
+            Employee employee = new Employee();
+            employee.setFirst_name("Juan");
+            employee.setPa_surname("Franco");
+            employee.setMa_surname("Rosas");
+            employee.setEmail("juan@gmail.com");
+            employee.setSalary((float) 5000);
+            repository.save(employee);
+            System.out.println("-----------LISTANDO EMPLEADOS ACTUALES--------");
+            repository.findAll().forEach(System.out::println);*/
+            System.out.println("--------------------ACTUALIZANDO EMPLEADO------------------");
+            Employee employee = new Employee();
+            employee.setId(6);
+            employee.setFirst_name("Juan Felipe");
+            employee.setPa_surname("Franco");
+            employee.setMa_surname("Rosas");
+            employee.setEmail("juan@gmail.com");
+            employee.setSalary((float) 5000);
+            repository.save(employee);
+            System.out.println("-----------LISTANDO EMPLEADOS ACTUALES--------");
+            repository.findAll().forEach(System.out::println);
+
         }
 
 
