4a44cd71efe6fbf29ba02cac824b3bd787320e35
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-05-23 23:11:58 -0500

    [Crear Menu MensjaesApp]
    Creamos el menu de nuestra aplicacion que tiene los llamados desde el service

--- mensajes_app/src/main/java/com/felipe/Inicio.java
+++ mensajes_app/src/main/java/com/felipe/Inicio.java
@@ -1,51 +1,13 @@
 package com.felipe;
-import com.felipe.model.Usuario;
-import com.felipe.service.UserService;
+
+import com.felipe.service.MensajeService;
 
 import java.util.Scanner;
 
 public class Inicio {
-    static String MenuPrincipalMensajes = ("=================== \n"
-            + "\n Mini red social \n"
-            + "1. Registrarse \n"
-            + "2. Iniciar sesión \n"
-            + "3. salir \n");
     public static void main(String[] args) {
-        //create an instance of Scanner class to read user input
         Scanner sc = new Scanner(System.in);
-        MenuPrincipal(sc);
-        sc.close();
-    }
-
-    public static void MenuPrincipal(Scanner sc) {
         int opcion = 0;
-
-        //First Menu
-        do{
-            System.out.println(MenuPrincipalMensajes);
-            //Read user input
-            opcion = sc.nextInt();
-
-            switch (opcion){
-                case 1:
-                    UserService.crearUsuario(sc);
-                    break;
-                case 2:
-                    Usuario usuario = UserService.login(sc);
-                    if(usuario.getId_usuario() > 0){
-                        MenuSesion.menuSesion(usuario, sc);
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }while(opcion != 3);
-    }
-}
-
-
-
-        /*int opcion =0;
         do {
             System.out.println("-----------------------");
             System.out.println("Aplicacion de mensajes");
@@ -58,29 +20,26 @@ public class Inicio {
             //leemos la opcion del usuario
             opcion = sc.nextInt();
 
-            switch (opcion){
+            switch (opcion) {
                 case 1:
-                    MensajesService.crearMensaje();
+                    MensajeService.crearMensaje();
                     break;
                 case 2:
-                    MensajesService.listarMensajes();
+                    MensajeService.listarMensaje();
                     break;
                 case 3:
-                    MensajesService.borrarMensaje();
+                    MensajeService.borrarMensaje();
                     break;
                 case 4:
-                    MensajesService.editarMensaje();
+                    MensajeService.editarMensaje();
                     break;
                 default:
                     break;
             }
-        }while (opcion !=5);*/
+        } while (opcion != 5);
+    }
+
+}
 
 
-        /*Conexion conexion = new Conexion();
-        try(Connection cnx = conexion.get_connection()){
-
-        }catch (Exception e){
-            System.out.println(e);
-        }*/
 
