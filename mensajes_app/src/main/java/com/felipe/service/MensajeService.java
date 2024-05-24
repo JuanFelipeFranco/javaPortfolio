d9907dcd3e8ea3c66574e216d35817a2cff6585f
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-05-23 23:11:23 -0500

    [Crear Mensaje Service]
    Creamos la clase service que interactua con la clase DAO y el inicio

--- mensajes_app/src/main/java/com/felipe/service/MensajeService.java
+++ mensajes_app/src/main/java/com/felipe/service/MensajeService.java
@@ -2,29 +2,47 @@ package com.felipe.service;
 
 import com.felipe.dao.MensajeDAO;
 import com.felipe.model.Mensaje;
-import com.felipe.model.Usuario;
 
 import java.util.Scanner;
 
 //la capa intermedia entre el menu principal y la capa de acceso
 public class MensajeService {
-    public static void crearMensaje(Usuario usuario, Scanner sc) {
+    public static void crearMensaje() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Escribe tu mensaje");
-        String mensajeTexto = sc.next();
-        Mensaje mensaje = new Mensaje(mensajeTexto, usuario.getId_usuario());
+        String mensajeTexto = sc.nextLine();
+
+        System.out.println("Tu nombre");
+        String nombre = sc.nextLine();
+
+        Mensaje mensaje = new Mensaje();
+        mensaje.setMensaje(mensajeTexto);
+        mensaje.setAuthor_mensaje(nombre);
 
         MensajeDAO.crearMensajeDB(mensaje);
     }
     public static void listarMensaje() {
-        MensajeDAO.listarMensajeDB();
+       MensajeDAO.leerMensajeDB();
     }
-    public static void borrarMensaje(Usuario usuario, Scanner sc) {
-        System.out.println("indica el ID del mensaje a borrar");
+    public static void borrarMensaje() {
+        Scanner sc = new Scanner(System.in);
+       System.out.println("indica el ID del mensaje a borrar");
         int id_mensaje = sc.nextInt();
-        Mensaje mensaje = new Mensaje(id_mensaje,usuario.getId_usuario());
 
-        MensajeDAO.borrarMensajeDB(mensaje);
+        MensajeDAO.borrarMensajeDB(id_mensaje);
     }
 
+    public static void editarMensaje(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu nuevo mensaje");
+        String mensaje = sc.nextLine();
+
+        System.out.println("Indica el ID del mensaje a editar");
+        int id_mensaje = sc.nextInt();
+        Mensaje actualizacion = new Mensaje();
+        actualizacion.setId_mensaje(id_mensaje);
+        actualizacion.setMensaje(mensaje);
+        MensajeDAO.actualizarMensajeDB(actualizacion);
+    }
 
 }
